bab74ea21554920f5f34d3d6b2642700f4e2d113
Mike <dartdelux@gmail.com>
2020-08-02 20:41:49 +0200

    Init Project
    Signed-off-by: Mike <dartdelux@gmail.com>

--- authentication/bin/main.dart
+++ authentication/bin/main.dart
@@ -1,50 +1,20 @@
 import 'dart:io';
 import 'package:openid_client/openid_client.dart';
+import 'package:openid_client/openid_client_io.dart';
 
 main() async {
+  var uri = Uri.http('127.0.0.1:8085', '/auth/realms/dart_realm/protocol/openid-connect/auth');
+  var issuer = await Issuer.discover(uri);
+  var client = Client(issuer, 'dart-client');
 
-  // print a list of known issuers
-  print(Issuer.knownIssuers);
+  var scopes = List.of(['openid']);
 
-  // discover the metadata of the google OP
-  var issuer = await Issuer.discover("");
+  var authenticator = Authenticator(client,
+      scopes: scopes,
+      port: 4000);
 
-  // create a client
-  var client = Client(issuer, 'client_id', 'client_secret');
+  var c = await authenticator.authorize();
 
-  // create a credential object from authorization code
-  var c = client.createCredential(code: 'some received authorization code');
+  return await c.getUserInfo();
 
-  // or from an access token
-  c = client.createCredential(accessToken: 'some received access token');
-
-  // or from an id token
-  c = client.createCredential(idToken: 'some id token');
-
-  // get userinfo
-  var info = await c.getUserInfo();
-  print(info.name);
-
-  // get claims from id token if present
-  print(c.idToken?.claims?.name);
-
-  // create an implicit authentication flow
-  var f = Flow.implicit(client);
-
-  // or an explicit flow
-  f = Flow.authorizationCode(client);
-
-  // set the redirect uri
-  f.redirectUri = Uri.parse('http://localhost');
-
-  // do something with the authentication url
-  print(f.authenticationUri);
-
-  // handle the result and get a credential object
-  c = await f.callback({
-    'code': 'some code',
-  });
-
-  // validate an id token
-  var violations = await c.validateToken();
 }
